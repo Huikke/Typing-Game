034666c1378e42aa096bc48962819aae776f1d22
Lassi Chen <lachen@utu.fi>
2023-09-04 01:23:59 +0300

    Changed to japanese version, changed input system

--- Versions/game-og.py
+++ Versions/game-og.py
@@ -1,4 +1,4 @@
-# Hiragana game
+# Typing game
 
 import pygame
 import sys
@@ -7,23 +7,22 @@ import random
 pygame.init()
 screen = pygame.display.set_mode((800,800))
 clock = pygame.time.Clock()
-font = pygame.font.Font("C:\Windows\Fonts\msgothic.ttc", 500)
 pygame.display.set_caption("Typing Game")
 
 # Generate and display a letter into the game
 # Is ran every time player presses the correct button
 # Returns index which corresponds with pygame keyboard button for checking the answer
-def char_spawn(char=None):
-    chars = [("あ", "a"), ("い", "i"), ("う", "u"), ("え", "e"), ("お", "o")]
-    if char == None:
-        rand = random.randint(0,4)
-        char = chars[rand]
+def char_spawn(next, randchar=None):
+    chars = "????ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    if next:
+        randchar = random.randint(4,29)
+    char = chars[randchar]
 
-    char_surf = font.render(char[0], False, "Black")
+    char_surf = pygame.font.SysFont(None, 500).render(char, False, "Black")
     char_rect = char_surf.get_rect(center = (400, 400))
     screen.blit(char_surf, char_rect)
 
-    return char
+    return randchar
 
 # Update score by 1 and display it
 # Takes parameter score and return score with +1
@@ -50,9 +49,8 @@ def timer():
     return remaining_time
 
 # Gameplay variables
-score = 0
-next = False
-first = True
+score = -1
+next = True
 time = 30 # In seconds
 
 # Game loop
@@ -62,42 +60,19 @@ while True:
             pygame.quit()
             sys.exit()
 
-        # Generate the first character
-        if first == True:
-            answer = char_spawn()
-            first = False
-
-        # Handles keyboard presses
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                if answer[1] == "a":
-                    next = True
-            elif event.key == pygame.K_i:
-                if answer[1] == "i":
-                    next = True
-            elif event.key == pygame.K_u:
-                if answer[1] == "u":
-                    next = True
-            elif event.key == pygame.K_e:
-                if answer[1] == "e":
-                    next = True
-            elif event.key == pygame.K_o:
-                if answer[1] == "o":
-                    next = True
-
     # Screen background
     screen.fill((204, 190, 35))
 
     # If next is True, the game updates
     # Else blit the same view again
     if next == True:
-        answer = char_spawn()
+        answer = char_spawn(True)
         score = score_handler(score, True)
         next = False
     else:
-        answer = char_spawn(answer)
+        answer = char_spawn(False, answer)
         score = score_handler(score, False)
-
+    
     # When timer reaches zero, game ends
     remaining_time = timer()
     if remaining_time < 0:
@@ -105,5 +80,12 @@ while True:
         pygame.quit()
         sys.exit()
 
+    # Handles keyboard presses
+    keys = pygame.key.get_pressed()
+    for index, key in enumerate(keys):
+        if key != False:
+            if index == answer:
+                next = True
+    
     pygame.display.update()
     clock.tick(30)
